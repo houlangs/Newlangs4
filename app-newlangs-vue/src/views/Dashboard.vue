abcc4bfc0596b1996644aa881a5c0b62c30e85bc
houalngs <g_jung@mail.harpercollege.edu>
2023-11-11 15:09:52 +0800

    Update Dashboard.vue

--- app-newlangs-vue/src/views/Dashboard.vue
+++ app-newlangs-vue/src/views/Dashboard.vue
@@ -1,5 +1,12 @@
+<style scoped>
+.button-group {
+  display: flex;
+  gap: 10px;
+  /* 调整间距大小 */
+}
+</style>
 <template>
-    <Toast /> ̰
+  <Toast /> ̰
   <div class="grid">
     <div class="col-12 xl:col-6">
       <div class="card">
@@ -10,19 +17,15 @@
         <h5 class="text-600">
           🎉厚浪域名解析永久免费，欢迎使用！<br />
           🤔使用时遇到问题，可前往<a href="https://check.langs.ink/status/main" target="_blank">系统状态</a>页面查看系统状态<br />
-        </h5>  
+        </h5>
         <br />
         <span class="block text-600 font-medium mb-3">🏆常用功能</span>
-        <h6>
-          <Button label="📅每日签到：签到有礼，节日积分加倍！" @click="signIn" />
-          <br>
-          <br>
+        <h6 class="button-group">
+          <Button label="📅每日签到" @click="signIn" />
           <Button
-            onclick="window.open('http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=73T48TWm3nzVt5jSF6SyDVY2AoyZfMSu&authKey=As1WSDrOpm3nmJNkH6q1OQ3r%2FkyOD24hR7OBXDs0MkfQD5be1hTQo2HU8LRUNn57&noverify=0&group_code=230832864')">📢加入Q群：交流技术，探讨问题</Button>
-          <br>
-          <br>
-          <Button
-            onclick="window.open('https://forum.houlangs.com/d/6-hou-lang-geng-duo-lian-jie')">🔗友链：探索更多有趣网页</Button>
+            onclick="window.open('http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=73T48TWm3nzVt5jSF6SyDVY2AoyZfMSu&authKey=As1WSDrOpm3nmJNkH6q1OQ3r%2FkyOD24hR7OBXDs0MkfQD5be1hTQo2HU8LRUNn57&noverify=0&group_code=230832864')">📢加入Q群</Button>
+          <Button onclick="window.open('https://forum.houlangs.com/d/6-hou-lang-geng-duo-lian-jie')">🔗友链</Button>
+          <Button onclick="window.open('https://langsteam.feishu.cn/share/base/form/shrcn708g5ZIScs87gvWo0ms8yc')">参与新功能调查</Button>
           <br>
         </h6>
       </div>
@@ -36,16 +39,16 @@ import { useToast } from "primevue/usetoast"
 const toast = useToast()
 
 const signIn = () => {
-    axios.get('/user/sign_in')
-        .then(function (response) {
-            if (response.data.code === 200) {
-                toast.add({ severity: 'success', summary: '签到成功', detail: '您已签到成功', life: 3000 })
-            } else {
-                console.log(response.data)
-                toast.add({ severity: 'error', summary: '签到失败', detail: '明天再来吧~', life: 3000 })
-            }
-        }).catch(function (error) {
-            console.log(error)
-        })
+  axios.get('/user/sign_in')
+    .then(function (response) {
+      if (response.data.code === 200) {
+        toast.add({ severity: 'success', summary: '签到成功', detail: '您已签到成功', life: 3000 })
+      } else {
+        console.log(response.data)
+        toast.add({ severity: 'error', summary: '签到失败', detail: '明天再来吧~', life: 3000 })
+      }
+    }).catch(function (error) {
+      console.log(error)
+    })
 }
 </script>
